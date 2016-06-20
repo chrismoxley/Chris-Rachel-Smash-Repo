fd36f5cb1a20aa52ad5e90d7ad502b8c882abfdc
CHRIS MOXLEY <CHRISTOPHER.MOXLEY@DUKE.EDU>
2016-06-20 23:09:35 +0200

    Log in done and challenged one complete

--- script.js
+++ script.js
@@ -13,42 +13,67 @@ app.config(function($routeProvider) {
   })
 });
 
-app.controller("LoginCtrl", function($scope, $location, $firebaseAuth) {
+app.controller("LoginCtrl", function($scope, $location, $firebaseAuth, $firebaseArray, $firebaseObject, $timeout) {
+  console.log(users)
   var auth = $firebaseAuth();
-
+  var ref = firebase.database().ref().child("users");
+  var users = {};
   auth.$onAuthStateChanged(function(firebaseUser) {
-
     if (firebaseUser) {
-          console.log(firebaseUser);
+      $scope.firebaseUser = firebaseUser;
       $location.path("/");
+        console.log(firebaseUser)
+        console.log(firebaseUser.uid)
+        console.log(firebaseUser.displayName)
+    users = {
+      uid: firebaseUser.uid,
+      name: firebaseUser.displayName
+    }
+
+    console.log(users)
+    } else {
+      $location.path("/login");
     }
   });
-
   $scope.signIn = function() {
     $scope.message = "";
     $scope.error = "";
     auth.$signInWithPopup("facebook")
-      .then(function(result) {
-        console.log(result);
-      })
-      .catch(function(error) {
-        $scope.error = error;
-        console.log(error);
-      });
+    .catch(function(error) {
+      $scope.error = error;
+    });
   }
 
 });
 
-app.controller("HomeCtrl", function($scope, $http, $route) {
+app.controller("HomeCtrl", function($scope, $http, $location, $firebaseAuth) {
   //home page angular here
-    $scope.logout = function() {
+  var auth = $firebaseAuth();
+  auth.$onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+      $scope.firebaseUser = firebaseUser;
+      
+    } else {
+      
+      $location.path("/login");
+    }
+  });
+  $scope.logout = function() {
     auth.$signOut();
     $location.path("/login");
   }
-	});
+});
+
 
 
 app.controller("MessageCtrl", function($scope, $http, $route) {
-  //Message page angular here
-	});
-
+ //Message page angular here
+    $scope.success = true; 
+ $scope.clickCount = 0;
+ $scope.counter = function () {
+   $scope.clickCount = $scope.clickCount + 1;
+ if ($scope.clickCount >= 5) {
+  $scope.success = false;
+ }
+ }
+});
